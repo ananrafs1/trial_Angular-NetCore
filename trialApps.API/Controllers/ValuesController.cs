94175c1a008c0a0ea60c6e606d02d037600d689f
ananrafs1 <ananrafs1@gmail.com>
2018-09-04 20:56:55 +0700

    update separate services

--- trialApps.API/Controllers/ValuesController.cs
+++ trialApps.API/Controllers/ValuesController.cs
@@ -22,6 +22,7 @@ namespace trialApps.API.Controllers
         }
         // GET api/values
         
+        [AllowAnonymous]
         [HttpGet]
         public async Task<IActionResult> GetValues()
         {
